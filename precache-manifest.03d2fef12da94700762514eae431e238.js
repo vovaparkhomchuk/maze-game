873dffbfd7d433fecfb3f880195252d68af13c51
Vova Parkhomchuk <vovaparkhomchuk@gmail.com>
2020-06-28 21:52:57 +0300

    Updates

---
+++ precache-manifest.03d2fef12da94700762514eae431e238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455ed705d2d6af00ef0ea2ade09733d6",
+    "url": "/maze-game/index.html"
+  },
+  {
+    "revision": "0f01b38fc3e175cd8f01",
+    "url": "/maze-game/static/css/main.629ebfbc.chunk.css"
+  },
+  {
+    "revision": "d3e8550411373deb8945",
+    "url": "/maze-game/static/js/2.97f36cfc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/maze-game/static/js/2.97f36cfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f01b38fc3e175cd8f01",
+    "url": "/maze-game/static/js/main.171d1ea6.chunk.js"
+  },
+  {
+    "revision": "63e639852001bd79fa6a",
+    "url": "/maze-game/static/js/runtime-main.fe6361a2.js"
+  }
+]);
